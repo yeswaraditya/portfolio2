273422cb65f7e8e00b54c9b7e2aa4c3d50e81c6a
Y Eswar Aditya <yeswaraditya3006@gmail.com>
2024-07-28 21:38:38 +0530

    added big profile pic

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
       colors: {
         customOrange: '#FF6D00',
       },
+      fontFamily: {
+        gimletDisplay: ['Gimlet Display', 'serif'],
+        gimletDisplayCompressed: ['Gimlet Display Compressed', 'serif'],
+        gimletDisplayNarrow: ['Gimlet Display Narrow', 'serif'],
+      },
     },
   },
 }
